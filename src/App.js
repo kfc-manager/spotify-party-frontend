4928c3ffd1e4f4020594ba3b40b7e29be2066106
kalle <111873492+kfc-manager@users.noreply.github.com>
2022-12-08 16:14:41 +0100

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Queue from "./components/Queue/Queue";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import Search from "./components/Search/Seach";
 
 function queueIndexing(queue) {
   for (let i = 0; i < queue.length; i++) {
@@ -14,14 +17,25 @@ export default function App() {
   const [queue, setQueue] = useState([]);
 
   useEffect(() => {
-    axios.get('/queue/get')
-    .then((response) => setQueue(queueIndexing(response.data)))
-    .catch((error) => {
-       //TODO handle error
-      });
+  axios.get("/queue/get")
+  .then((response) => setQueue(queueIndexing(response.data)))
+  .catch((error) => {
+      //TODO handle error
+    });
   }, []);
 
   return (
-    <Queue queue={queue}/>
-  );
+    <div>
+      <nav className="navbar">
+        <ul className="test">
+            <Link className="queue" to="/">Queue</Link>
+            <Link className="search" to="/addtrack">Add Song</Link>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Queue queue={queue}/>}/>
+        <Route path="/addtrack" element={<Search/>}/>
+      </Routes>
+    </div>);
+
 }
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,14 +1,14 @@
-import './Track.css';
+import "./Track.css";
 
 function toMinutes(duration_ms) {
   let minutes = Math.floor(duration_ms/60000);
-  let seconds = (duration_ms%60000) + '';
+  let seconds = (duration_ms%60000) + "";
 
   while (seconds.length < 5) {
-    seconds = '0' + seconds;
+    seconds = "0" + seconds;
   }
 
-  return minutes + ':' + seconds.slice(0,2);
+  return minutes + ":" + seconds.slice(0,2);
 }
 
 function artistString(artists) {
@@ -21,13 +21,13 @@ function artistString(artists) {
 
 export default function Track({ track }) {
   return (
-    <div className='track'>
-      <img className='icon' src={ track.image } alt=''/>
-      <div className='container'>
-        <div className='name'>{ track.name }</div>
-        <div className='artists'>{ artistString(track.artists) }</div>
+    <div className="track">
+      <img className="icon" src={ track.image } alt=''/>
+      <div className="container">
+        <div className="name">{ track.name }</div>
+        <div className="artists">{ artistString(track.artists) }</div>
       </div>
-      <div className='duration'>{ toMinutes(track.duration) }</div>
+      <div className="duration">{ toMinutes(track.duration) }</div>
     </div>
   )
 }
